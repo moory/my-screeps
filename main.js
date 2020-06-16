cdad52dc2cc27912f95da4b0dad0f5cd407703ab
马积力 <majili@druid.tech>
2020-06-16 16:39:57 +0800

    格式优化

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
-const HOME=Game.spawns['Spawn1'];
-const ROOM=Game.rooms['W18S23'];
-const CREEP_TOTAL=13;
-const CREEP_NAME='Worker';
+const HOME = Game.spawns['Spawn1'];
+const ROOM = Game.rooms['W18S23'];
+const CREEP_TOTAL = 13;
+const CREEP_NAME = 'Worker';
 
 
 module.exports.loop = function () {
+    let creepsCount = 0
 
-    born();
-    for(let k in Game.creeps){
-        if(k==='Worker0'||k==='Worker1'||k==='Worker3'||k==='Worker4'||k==='Worker5'){
-            work(Game.creeps[k],HOME,0)
-        }else if(k==='Worker2'){
+    for (const i in Game.creeps) {
+        creepsCount++;
+    }
+
+    if (creepsCount < CREEP_TOTAL) {
+        born();
+    }
+
+    for (let k in Game.creeps) {
+        if (k === 'Worker0' || k === 'Worker1' || k === 'Worker3' || k === 'Worker4' || k === 'Worker5') {
+            work(Game.creeps[k], HOME, 0)
+        } else if (k === 'Worker99') {
             build(Game.creeps[k]);
-        }else if(k==='Worker12'){
+        } else if (k === 'Worker99') {
             repair(Game.creeps[k])
-        }else{
+        } else {
             upgradeRCL(Game.creeps[k])
         }
 
@@ -25,7 +33,7 @@ module.exports.loop = function () {
 /*
 * 爆兵
 * */
-const born=()=>{
+const born = () => {
     if (!Game.creeps["Worker0"]) {
         Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Worker0');
         return;
@@ -84,19 +92,18 @@ const born=()=>{
 /*
 * 工作（采集，home溢出时升级RCL）
 * */
-const work=(c,t,s)=>{
-    if(c.carry.energy < c.carryCapacity) {
-        let source=nearGold(c);
-        if(c.harvest(source) === ERR_NOT_IN_RANGE) {
+const work = (c, t, s) => {
+    if (c.carry.energy < c.carryCapacity) {
+        let source = nearGold(c);
+        if (c.harvest(source) === ERR_NOT_IN_RANGE) {
             c.moveTo(source);
         }
-    }
-    else {
+    } else {
         // if(t.energy===t.energyCapacity){
         //     upgradeRCL(c);
         //     return;
         // }
-        if( c.transfer(t, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE ) {
+        if (c.transfer(t, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
             c.moveTo(t);
         }
     }
@@ -105,27 +112,27 @@ const work=(c,t,s)=>{
 /*
 * 建造
 * */
-const build=(c)=>{
-    for(let k in Game.constructionSites){
-        let target= Game.constructionSites[k];
-        let source=nearGold(c);
-        if(c.carry.energy===0){
+const build = (c) => {
+    for (let k in Game.constructionSites) {
+        let target = Game.constructionSites[k];
+        let source = nearGold(c);
+        if (c.carry.energy === 0) {
 
-            if(c.harvest(source) === ERR_NOT_IN_RANGE) {
+            if (c.harvest(source) === ERR_NOT_IN_RANGE) {
                 c.moveTo(source);
             }
-        }else if(c.carry.energy === c.carryCapacity){
-            if( c.build(target) === ERR_NOT_IN_RANGE ) {
+        } else if (c.carry.energy === c.carryCapacity) {
+            if (c.build(target) === ERR_NOT_IN_RANGE) {
                 c.moveTo(target);
             }
-        }else if(calcPath(target,c)>calcPath(source,c)){//离矿更近
+        } else if (calcPath(target, c) > calcPath(source, c)) {//离矿更近
 
             let sources = c.room.find(FIND_SOURCES);
-            if(c.harvest(source) === ERR_NOT_IN_RANGE) {
+            if (c.harvest(source) === ERR_NOT_IN_RANGE) {
                 c.moveTo(source);
             }
-        }else{
-            if( c.build(target) === ERR_NOT_IN_RANGE ) {
+        } else {
+            if (c.build(target) === ERR_NOT_IN_RANGE) {
                 c.moveTo(target);
             }
         }
@@ -137,23 +144,23 @@ const build=(c)=>{
 /*
 * 升级RCL
 * */
-const upgradeRCL=(c)=>{
-    let source=nearGold(c);
+const upgradeRCL = (c) => {
+    let source = nearGold(c);
 
-    if(c.carry.energy===0){
-        if(c.harvest(source) === ERR_NOT_IN_RANGE) {
+    if (c.carry.energy === 0) {
+        if (c.harvest(source) === ERR_NOT_IN_RANGE) {
             c.moveTo(source);
         }
-    }else if(c.carry.energy === c.carryCapacity){
-        if( c.upgradeController(ROOM.controller) === ERR_NOT_IN_RANGE ) {
+    } else if (c.carry.energy === c.carryCapacity) {
+        if (c.upgradeController(ROOM.controller) === ERR_NOT_IN_RANGE) {
             c.moveTo(ROOM.controller);
         }
-    }else if(calcPath(ROOM.controller,c)>calcPath(source,c)){//离矿更近
-        if(c.harvest(source) === ERR_NOT_IN_RANGE) {
+    } else if (calcPath(ROOM.controller, c) > calcPath(source, c)) {//离矿更近
+        if (c.harvest(source) === ERR_NOT_IN_RANGE) {
             c.moveTo(source);
         }
-    }else{
-        if( c.upgradeController(ROOM.controller) === ERR_NOT_IN_RANGE ) {
+    } else {
+        if (c.upgradeController(ROOM.controller) === ERR_NOT_IN_RANGE) {
             c.moveTo(ROOM.controller);
         }
     }
@@ -161,27 +168,27 @@ const upgradeRCL=(c)=>{
 }
 
 /*维修*/
-const repair=(c)=>{
-    for(let k in Game.structures){
-        let target= Game.structures[k];
-        let source=nearGold(c);
-        if(c.carry.energy===0){
+const repair = (c) => {
+    for (let k in Game.structures) {
+        let target = Game.structures[k];
+        let source = nearGold(c);
+        if (c.carry.energy === 0) {
 
-            if(c.harvest(source) === ERR_NOT_IN_RANGE) {
+            if (c.harvest(source) === ERR_NOT_IN_RANGE) {
                 c.moveTo(source);
             }
-        }else if(c.carry.energy === c.carryCapacity){
-            if( c.repair(target) === ERR_NOT_IN_RANGE ) {
+        } else if (c.carry.energy === c.carryCapacity) {
+            if (c.repair(target) === ERR_NOT_IN_RANGE) {
                 c.moveTo(target);
             }
-        }else if(calcPath(target,c)>calcPath(source,c)){//离矿更近
+        } else if (calcPath(target, c) > calcPath(source, c)) {//离矿更近
 
             let sources = c.room.find(FIND_SOURCES);
-            if(c.harvest(source) === ERR_NOT_IN_RANGE) {
+            if (c.harvest(source) === ERR_NOT_IN_RANGE) {
                 c.moveTo(source);
             }
-        }else{
-            if( c.repair(target) === ERR_NOT_IN_RANGE ) {
+        } else {
+            if (c.repair(target) === ERR_NOT_IN_RANGE) {
                 c.moveTo(target);
             }
         }
@@ -193,11 +200,11 @@ const repair=(c)=>{
 /*
 * 计算最近的金矿
 * */
-const nearGold=(c)=>{
+const nearGold = (c) => {
     let sources = ROOM.find(FIND_SOURCES);
-    if(calcPath(sources[0],c)<calcPath(sources[1],c)){
+    if (calcPath(sources[0], c) < calcPath(sources[1], c)) {
         return sources[0]
-    }else{
+    } else {
         return sources[1]
     }
 
@@ -206,11 +213,11 @@ const nearGold=(c)=>{
 /*
 * 距离量算
 * */
-const calcPath=(t,c)=>{
-    return PathFinder.search(t.pos,c).cost
+const calcPath = (t, c) => {
+    return PathFinder.search(t.pos, c).cost
 }
 
-const findCreeps=(name)=>{
+const findCreeps = (name) => {
     return Game.creeps[name]
 }
 
