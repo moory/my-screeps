372e921ab5b4933fc9f73ad411e688e7ad92fdeb
马积力 <majili@druid.tech>
2025-04-29 16:52:55 +0800

    update: 调整修墙阈值

--- dist/main.js
+++ dist/main.js
@@ -146,34 +146,35 @@ var role_upgrader = {
 };
 
 var roleRepairer$1 = {
-  run(creep) {
-    if (creep.store[RESOURCE_ENERGY] === 0) {
-      const source = creep.pos.findClosestByPath(FIND_SOURCES);
-      if (source) {
-        if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
-          creep.moveTo(source, { visualizePathStyle: { stroke: '#00ff00' } });
-        }
-      }
-    } else {
-      const target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-        filter: s =>
-          (s.structureType === STRUCTURE_WALL || s.structureType === STRUCTURE_RAMPART) &&
-          s.hits < 50000 // 你可以根据房间 RCL 设定更高阈值
-      });
+    run(creep) {
+        if (creep.store[RESOURCE_ENERGY] === 0) {
+            const source = creep.pos.findClosestByPath(FIND_SOURCES);
+            if (source) {
+                if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source, { visualizePathStyle: { stroke: '#00ff00' } });
+                }
+            }
+        } else {
+            const target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: s =>
+                    (s.structureType === STRUCTURE_ROAD || s.structureType === STRUCTURE_WALL) &&
+                    s.hits < s.hitsMax &&  // 修复所有不满血建筑
+                    (s.structureType !== STRUCTURE_WALL || s.hits < 5000) // 墙壁只修到 5000
+            });
 
-      if (target) {
-        if (creep.repair(target) === ERR_NOT_IN_RANGE) {
-          creep.moveTo(target, { visualizePathStyle: { stroke: '#ff00ff' } });
+            if (target) {
+                if (creep.repair(target) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target, { visualizePathStyle: { stroke: '#ff00ff' } });
+                }
+            } else {
+                // 没事干时靠近 controller 待命
+                const controller = creep.room.controller;
+                if (controller) {
+                    creep.moveTo(controller);
+                }
+            }
         }
-      } else {
-        // 没事干时靠近 controller 待命
-        const controller = creep.room.controller;
-        if (controller) {
-          creep.moveTo(controller);
-        }
-      }
-    }
-  },
+    },
 };
 
 var role_repairer = roleRepairer$1;
