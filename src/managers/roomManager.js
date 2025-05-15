d2ffb5e45479a10c706399bcd182dafd2b49b618
马积力 <majili@druid.tech>
2025-05-15 10:43:49 +0800

    feat(roomManager): 添加建筑管理模块以自动规划房间建筑
    建筑管理模块负责自动规划和建造房间内的建筑，包括扩展、防御塔、存储、终端和实验室等。该模块根据房间控制器的等级和现有建筑情况，智能地规划并创建建筑工地，提升房间的自动化管理能力。

--- src/managers/roomManager.js
+++ src/managers/roomManager.js
@@ -2,6 +2,7 @@ const creepManager = require('./creepManager');
 const structureManager = require('./structureManager');
 const defenseManager = require('./defenseManager');
 const spawnManager = require('./spawnManager');
+const constructionManager = require('./constructionManager');
 
 module.exports = {
   run(room) {
@@ -9,5 +10,6 @@ module.exports = {
     creepManager.run(room);
     structureManager.run(room);
     defenseManager.run(room);
+    constructionManager.run(room);
   },
 };
