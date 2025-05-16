baa0eb90ab7e56f6def1a26b8ee46a2ecaebe9d3
马积力 <majili@druid.tech>
2025-05-16 19:54:39 +0800

    feat: 添加dismantler1 creep的目标房间和敌对结构逻辑
    为dismantler1 creep添加了移动到目标房间和攻击敌对结构的逻辑。当creep不在目标房间时，会寻找出口并移动；在目标房间时，会寻找最近的敌对结构并进行拆除。

--- src/main.js
+++ src/main.js
@@ -16,6 +16,19 @@ module.exports.loop = function () {
     
     // 清理内存
     memoryManager.run();
+
+    const creep = Game.creeps['dismantler1'];
+if (creep.room.name !== creep.memory.targetRoom) {
+  const exit = creep.room.findExitTo(creep.memory.targetRoom);
+  creep.moveTo(creep.pos.findClosestByRange(exit));
+} else {
+  const target = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
+  if (target) {
+    if (creep.dismantle(target) === ERR_NOT_IN_RANGE) {
+      creep.moveTo(target);
+    }
+  }
+}
     
     // 根据当前模式执行不同逻辑
     const currentMode = configManager.getMode();
